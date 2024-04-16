3c3dc980b32e514869e4e9d4d85181ce5b398d40
Ryan Chen <ryan.chen.poyu@gmail.com>
2024-04-16 17:50:03 -0400

    Make file and  port not global variables

--- src/purdec.c
+++ src/purdec.c
@@ -1,31 +1,55 @@
 #include <gcrypt.h>
+#include <stdlib.h>
+#include <sys/socket.h> 
+#include <unistd.h>
+#include <netinet/in.h>
 
 #include "purdec.h"
 
-char * filename;
-int port;
 
 int main(int argc, char **argv) {
-    parseArgv(argc, argv);
+    char password[BUFSIZE];
+    char * filename = NULL;
+    int port;
+
+    parseArgv(argc, argv, &port, &filename);
+
+
     return 0;
 }
 
-void displayHelp() {
-    printf("Usuage: purdec <port> [-l <input file>]\n");
+void promptPassword(char * password) {
+    printf("Password to decrypt the files under: ");
+    fgets(password, sizeof(password) - 1, stdin);
+    return;
 }
 
-void parseArgv(int argc, char **argv) {
+/*
+ * displayHelp: Show the correct usuage of the program
+ */
+void displayHelp() {
+    printf("Usuage: purdec [<port>] [-l <input file>]\n");
+}
+
+/*
+ * praseArgv
+ *     Parse command line arguements
+ *     Update Global variables 'port' and 'filename'
+ * argc: number of command line argument
+ * argv: command line arguments
+ */
+void parseArgv(int argc, char **argv, int * port, char ** filename) {
     if (argc == 2) {
-        port = atoi(argv[1]);
+        *port = atoi(argv[1]);
         return;
     }
-    if (argc == 4) {
-        if (strcmp("-l", argv[2])) {
+    if (argc == 3) {
+        if (strcmp("-l", argv[1])) {
             displayHelp();
             exit(0);
         }
-
-        filename = argv[3];
+        *port = -1;
+        *filename = argv[2];
         return;
     }
     displayHelp();
